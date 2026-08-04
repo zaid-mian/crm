6343bd200f664540e31c9af960ea6a66b770aa60
zaid-mian <mianzaid049@gmail.com>
2026-08-04 10:01:37 +0500

    Merge main into feature/dashboard

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/', include('leads.urls')),
     path('api/', include('contacts.urls')),
     path('api/', include('opportunities.urls')),
     path('api/', include('companies.urls')),
     path('api/', include('pipeline.urls')),
     path('api/', include('payments.urls')),
+
+    # ADD THESE
+    path('api/', include('roles.urls')),
+    path('api/', include('dashboard.urls')),
 ]
